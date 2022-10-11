3d72abcdc0d0ec544355bf5f890aff52b3b8baa4
Usuario <Usuario@DESKTOP-7V2OB67>
2022-10-11 22:45:37 +0200

    Añadiendo jUnit

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -1,9 +1,25 @@
 
 public class Calculadora {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int suma(int a, int b) {
+		return a+b;
 	}
+	
+	public static int Resta(int a, int b) {
+		return a-b;
+	}
+	
+	public static int Multiplica(int a, int b) {
+		return a*b;
+	}
+	
+	public static int Divide(int a, int b) {
+		if(b!=0) {
+			return a/b;
+		}else {
+			return -1;
+		}
+	}
+	
 
 }
