9c458abe02cf95737a4b093dbfb43e4424e5bb45
Usuario <Usuario@DESKTOP-7V2OB67>
2022-10-12 11:18:03 +0200

    Añadiendo la clase Empleado

---
+++ SegundaParte/src/Empleado.java
@@ -0,0 +1,15 @@
+
+public class Empleado {
+
+	enum TipoEmpleado{Hombre, Mujer};
+	
+	public float calculoNominaBruta(TipoEmpleado tipo, float ventasMes, float horasExtra) {
+		
+		
+	}
+	
+	 public float calculoNominaNeta(float nominaBruta) {
+		 
+		 
+	 }
+}
